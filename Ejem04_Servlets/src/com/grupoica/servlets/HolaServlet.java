1447425780a48756e019605e01452f9020e0580c
pabloferrer <pablo@certrans.es>
2020-10-30 11:20:16 +0100

    Ejercicio imprimir por POST tantas veces como el valor del GET

---
+++ Ejem04_Servlets/src/com/grupoica/servlets/HolaServlet.java
@@ -0,0 +1,59 @@
+package com.grupoica.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HolaServlet
+ */
+@WebServlet("/hola")
+public class HolaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HolaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String strNombre = request.getParameter("nombre");
+		
+		String html = "<html><head><title>Formulario envio</title></head><body>";
+		if(strNombre == null|| "".equals(strNombre) ){
+			html += "<h2>Pon el nombre</h2>";
+		}else {
+			html+="<form action='./hola.do' method='post'>" +
+					"	Veces: <input name='veces' type='number'/>" +
+					"	<input type='submit' value='POST'/>" + 
+					"</form>";
+		}
+		html+="</body></html>";
+				
+		response.getWriter().append(html).append(request.getContextPath());
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("<h1>Hola desde servlet</h1> ").append(request.getContextPath());
+		String veces = request.getParameter("veces");
+		
+		for(int i = 0;i<Integer.parseInt(veces);i++) {
+			response.getWriter().append("<p>" + i + "vez</p> ").append(request.getContextPath());
+		}
+		
+	}
+	
+	
+	
+	
+}
